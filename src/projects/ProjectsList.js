4712a71d70112d6a63453638fcfb98a3d18eb367
Daivikh Rajesh <darkdevill7799@gmail.com>
2023-12-13 01:06:15 -0500

    Project list updated.

--- src/projects/ProjectsList.js
+++ src/projects/ProjectsList.js
@@ -1,49 +1,69 @@
 export const ProjectsList = [
-    {
-        id: 1,
-        repoName: 'Data_Insights_Using_Tableau',
-        description: 'Developed interactive Tableau dashboard for sales data analysis, enabling data-driven decision-making and revenue growth insights.',
-        liveUrl: 'https://github.com/daivikhrajesh/Data_Insights_Using_Tableau',
-        hashTags:['MySQL', 'Tableau'],
-
-    },
-    {
-        id: 2,
-        repoName: 'Service Desk Application',
-        description: `Developed a centralized, user-friendly web tool with role-based access control, email notifications, and Power BI integration, 
+  {
+    id: 1,
+    repoName: "Data_Insights_Using_Tableau",
+    description:
+      "Developed interactive Tableau dashboard for sales data analysis, enabling data-driven decision-making and revenue growth insights.",
+    liveUrl: "https://github.com/daivikhrajesh/Data_Insights_Using_Tableau",
+    hashTags: ["MySQL", "Tableau"],
+  },
+  {
+    id: 2,
+    repoName: "Service Desk Application",
+    description: `Developed a centralized, user-friendly web tool with role-based access control, email notifications, and Power BI integration, 
         improving query management and user experience.`,
-        liveUrl: 'https://github.com/daivikhrajesh/Service-Desk-Application-Project',
-        hashTags:[' C#', 'CSS', 'Html','PowerBI','ReactJS'],
-    },
-    {
-        id: 3,
-        repoName: 'Spatial Data Analysis',
-        description: 'Developed a Node.js web app for GIS data visualization, utilizing PostgreSQL and Leaflet. Displayed geographic data on a map from a PostgreSQL database.',
-        liveUrl: 'https://youtu.be/8I_RHimEq_c',
-        hashTags:['PostgreSQL', 'NodeJs','Html'],
-    },
-    {
-        id: 4,
-        repoName: 'Healthcare-Readmission-Prediction',
-        description: 'This healthcare readmission prediction project utilizes Python for data analysis and machine learning.',
-        liveUrl: 'https://github.com/daivikhrajesh/Healthcare-Readmission-Prediction',
-        hashTags:['Python', 'Pandas','EDA','Random Forest Classifier'],
-    },
-    {
-        id: 5,
-        repoName: 'House-Prediction',
-        description: 'This toolkit streamlines data preprocessing and visualization, using pandas, numpy, seaborn, and matplotlib.',
-        liveUrl: 'https://github.com/daivikhrajesh/House-Prediction-EDA',
-        hashTags:['Python', 'Pandas', 'matplotlib'],
-    },
-    {
-        id:5,
-        repoName:'BCI Controlled Wheelchair with Home Automation and Health Care unit',
-        description :' Developed a Brain-Computer Interface (BCI) to synthesize brain signals for wheelchair movement. Also incorporated a health monitoring unit and enabled home automation controlled by the BCI.',
-        liveUrl : 'https://www.youtube.com/watch?v=gWSeAIdvJXw',
-        hashTags:['BCI','EEG','GSM module','IOT'],
-    },
-    
-]
-    
-    
+    liveUrl:"https://github.com/daivikhrajesh/Service-Desk-Application-Project",
+    hashTags: [" C#", "CSS", "Html", "PowerBI", "ReactJS"],
+  },
+  {
+    id: 3,
+    repoName: "Spatial Data Analysis",
+    description:
+      "Developed a Node.js web app for GIS data visualization, utilizing PostgreSQL and Leaflet. Displayed geographic data on a map from a PostgreSQL database.",
+    liveUrl: "https://youtu.be/8I_RHimEq_c",
+    hashTags: ["PostgreSQL", "NodeJs", "Html", "GIS"],
+  },
+  {
+    id: 4,
+    repoName: "Healthcare-Readmission-Prediction",
+    description:
+      "This healthcare readmission prediction project utilizes Python for data analysis and machine learning.",
+    liveUrl:
+      "https://github.com/daivikhrajesh/Healthcare-Readmission-Prediction",
+    hashTags: ["Python", "Pandas", "EDA", "Random Forest Classifier"],
+  },
+  {
+    id: 5,
+    repoName: "House-Prediction",
+    description:
+      "This toolkit streamlines data preprocessing and visualization, using pandas, numpy, seaborn, and matplotlib.",
+    liveUrl: "https://github.com/daivikhrajesh/House-Prediction-EDA",
+    hashTags: ["Python", "Pandas", "matplotlib"],
+  },
+  {
+    id: 6,
+    repoName:
+      "Tata Consultancy Services(TCS), Data Visualization: Empowering Business with Effective Insights",
+    description:
+      " Utilized Python and Tableau to anticipate the requirements of C-level executives within the organization.  Presented Tableau dashboard to TCS analysts for cost-effective market analysis and strategic expansion insights.",
+    liveUrl: "https://github.com/daivikhrajesh/TCS_microinternship",
+    hashTags: ["Python", "Tableau", "Data Visualization", "EDA"],
+  },
+  {
+    id: 7,
+    repoName: "Portfolio Website",
+    description:
+      " It's a dynamic and responsive portfolio site built with React for the frontend and styled with Tailwind CSS.",
+    liveUrl: "https://github.com/daivikhrajesh/portfolio",
+    hashTags: ["Js", "React", "Html", "Tailwind CSS"],
+  },
+  {
+    id: 8,
+    repoName:
+      "BCI Controlled Wheelchair with Home Automation and Health Care unit",
+    description:
+      " Developed a Brain-Computer Interface (BCI) to synthesize brain signals for wheelchair movement. Also incorporated a health monitoring unit and enabled home automation controlled by the BCI.",
+    liveUrl: "https://www.youtube.com/watch?v=gWSeAIdvJXw",
+    hashTags: ["BCI", "EEG", "GSM module", "IOT"],
+  },
+];
