e8efcd65361519bdf5e25a39366ad827b0550de8
Daivikh Rajesh <darkdevill7799@gmail.com>
2023-12-14 22:56:01 -0500

    Updates

---
+++ precache-manifest.407893919441934337fda227ccf67c2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31acc536fe16597e937ebbe6606059fe",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "65d1e751544cfc4268b1",
+    "url": "/portfolio/static/css/main.3bb56689.chunk.css"
+  },
+  {
+    "revision": "ff1ada23a7ff795dc3d7",
+    "url": "/portfolio/static/js/2.a0def176.chunk.js"
+  },
+  {
+    "revision": "fe4cdd745462666d252711c3f664a082",
+    "url": "/portfolio/static/js/2.a0def176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65d1e751544cfc4268b1",
+    "url": "/portfolio/static/js/main.75a3c306.chunk.js"
+  },
+  {
+    "revision": "8e14fc87b56b2530cf68",
+    "url": "/portfolio/static/js/runtime-main.4eb52fc4.js"
+  },
+  {
+    "revision": "cb6d04df6fe9e1da2e241d06ba278812",
+    "url": "/portfolio/static/media/github.cb6d04df.svg"
+  },
+  {
+    "revision": "b79805d54ed60ec9e0fa485b3271ac96",
+    "url": "/portfolio/static/media/linkedin2.b79805d5.svg"
+  }
+]);
